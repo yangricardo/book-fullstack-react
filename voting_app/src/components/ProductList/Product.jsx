c6fdb5967f695d3f62ace10e4192ff7a394cf26b
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-08-12 22:46:54 -0300

    segment

--- voting_app/src/components/ProductList/Product.jsx
+++ voting_app/src/components/ProductList/Product.jsx
@@ -12,27 +12,29 @@ class Product extends Component {
 
   render() {
     return (
-      <div className="item">
-        <div className="image">
-          <img src={this.props.productImageUrl} />
-        </div>
-        <div className="middle aligned content">
-          <div className="header">
-            <a onClick={this.handleUpVote}>
-              <i className="large caret up icon" />
-            </a>
-            {this.props.votes}
+      <div className="ui clearing segment">
+        <div className="item">
+          <div className="ui small left floated image">
+            <img src={this.props.productImageUrl} />
           </div>
-          <div className="description">
-            <a href={this.props.url}>{this.props.title}</a>
-            <p>{this.props.description}</p>
-          </div>
-          <div className="extra">
-            <span>Submitted by:</span>
-            <img
-              className="ui avatar image"
-              src={this.props.submitterAvatarUrl}
-            />
+          <div className="middle aligned content">
+            <div className="header">
+              <a onClick={this.handleUpVote}>
+                <i className="large caret up icon" />
+              </a>
+              {this.props.votes}
+            </div>
+            <div className="description">
+              <a href={this.props.url}>{this.props.title}</a>
+              <p>{this.props.description}</p>
+            </div>
+            <div className="extra">
+              <span>Submitted by:</span>
+              <img
+                className="ui avatar image"
+                src={this.props.submitterAvatarUrl}
+              />
+            </div>
           </div>
         </div>
       </div>
